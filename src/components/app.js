fd96508d91682a785b1333a62b718fc344524b5d
Adam Cooper <Adam.Cooper@ynap.com>
2017-10-20 16:28:09 +0100

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import OverlaySection from './OverlaySection.js';
+
+var Info = require('../messages/info.json');
 
 export default class App extends Component {
+
   render() {
+
+    console.log(Info["Webpack"]);
+
     return (
-      <div>React simple starter</div>
+      <div>
+        <h3>Hello World</h3>
+        <OverlaySection componentName='Component' deets={Info["Webpack"]}/>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+    },
+    {
+          test: /\.json$/,
+          loader: 'json-loader'
+        }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
